efc464d5d72de894ab73ca1d19d9783e0f75cbda
Ahmed <ahmedkhozam8085@gmail.com>
2025-02-01 09:10:21 +0200

    This is the first commit.

---
+++ src/main/java/com/hogwarts/HogwartsArifactsOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.hogwarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsArifactsOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsArifactsOnlineApplication.class, args);
+	}
+
+}
